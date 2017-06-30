6332c74c0f32d6b3b57ca05a3ce38f65093779d5
Dennis Iversen <dennis.iversen@gmail.com>
2017-06-30 17:05:23 +0200

    auto commit

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # point-on-circle
 
-Simple method to find a point on a circle. 
+Simple method to find a point on a circle calculated with a center point
 
 npm install --save point-on-circle
+
+# Code
+
+See [index.js](index.js)
 
 # Usage
 
 See [test.js](test.js)
 
-License: MIT# point-on-circle
+MIT © [Dennis Iversen](https://github.com/diversen)
+
--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 var circlePoint = require('./index.js');
 
-// Radians to move around the center point
-var radians = Math.PI/4;
+// Change above statement to
+// var circlePoint = require('point-on-circle');
+
+// num Radians to move. E.g. 1/4 of a circle
+var radians = Math.PI/2;
 
 // Center point in x, y
 var center = {x: 200, y: 200};
 
-// Point to calculate next point from
+// Point to calculate next point from. This also defines a circle
+// around the center point
 var point = {x: 300, y: 200};
 
 var nextPoint = circlePoint(point, center, radians);
 
 console.log(nextPoint);
+// => { x: 200, y: 300 } 
