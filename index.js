1038f2440fecf627c86f8010354ae1064d5f8481
Dennis Iversen <dennis.iversen@gmail.com>
2017-06-30 17:09:34 +0200

    better name of function to be exported

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function rotatePoint(cords, center, theta) {
+function pointOnCircle(cords, center, theta) {
 	var x = Math.cos(theta) * (cords.x-center.x) - Math.sin(theta) * (cords.y-center.y) + center.x
 	var y = Math.sin(theta) * (cords.x-center.x) + Math.cos(theta) * (cords.y-center.y) + center.y
 	return {x: x, y: y};
 }
 
-module.exports = rotatePoint;
+module.exports = pointOnCircle;
